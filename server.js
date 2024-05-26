e76ed31c1de945655e48d503518743f30c81b050
Tomás Arrativel <arrativeltomas@gmail.com>
2024-05-26 17:02:27 -0300

    Mounting the apiRouter and Listening the server on the PORT variable

--- server.js
+++ server.js
@@ -16,8 +16,12 @@ app.use(cors());
 app.use(bodyParser.json());
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
+app.use('/api', apiRouter);
 
 // This conditional is here for testing purposes:
 if (!module.parent) {
   // Add your code to start the server listening at PORT below:
+  app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+  });
 }
